610683318a3ffa475b73510c2496298c53b465fd
Sasirekha Kambhampaty <ksasirekha@gmail.com>
2020-09-12 11:45:10 +0530

    All auto layout changes

--- Swoosh/ViewController.swift
+++ Swoosh/ViewController.swift
@@ -18,9 +18,12 @@ class ViewController: UIViewController {
         
         
         // A view's frame is always relative to its parent's position
-        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
-        
-        bgImg.frame = view.frame
+//        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+//
+//        bgImg.frame = view.frame
+//
+//
+        // It is recommended by apple to use AutoLayout instead of managing this programmatically.
         
     }
 }
