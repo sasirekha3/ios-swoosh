7a793e6dff30fea0494a3edf8be282b0ee327033
Sasirekha Kambhampaty <ksasirekha@gmail.com>
2020-09-13 20:50:07 +0530

    conditional segue enabling

--- Swoosh/Controller/LeagueViewController.swift
+++ Swoosh/Controller/LeagueViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class LeagueViewController: UIViewController {
     
+    // The exclamatory implies that we don't want this code to even run if there is no value in player
+    var player: Player!
+    
+    
+    
     @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
         
     }
@@ -18,9 +23,36 @@ class LeagueViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        player = Player()
     }
     
-
+    // This segue was created by dragging one ctrl+dragging one VC to another (as opposed to the one made using the Interface Builder, where a button was ctrl+dragged onto the next VC)
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillViewControllerSegue", sender: self)
+    }
+    
+    // We need an IBOutlet to refere to the Next button and make changes
+    // Make sure IBOutlet variables are weak so that the memory of that button is released and doesn't result in memory leaks or uncollected references.
+    @IBOutlet weak var nextBtn: BorderButton!
+    
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String){
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ Swoosh/Model/Player.swift
@@ -0,0 +1,19 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Sasirekha Kambhampaty on 9/13/20.
+//  Copyright © 2020 Sasirekha Kambhampaty. All rights reserved.
+//
+
+import Foundation
+
+// Use a struct whenever you can - i.e. when using primitive types, etc. Struct is a value type and can be copied over directly. Whereas a class is an object and can be referenced but not copied easily. Structs are also faster.
+
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
+
